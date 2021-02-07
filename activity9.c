5cc52d0036f213a89e0e3415167a33551b3924e3
ENG19CS0006-Abhay <76053889+ENG19CS0006-Abhay@users.noreply.github.com>
2021-02-07 10:57:20 +0530

    Update activity9.c

--- activity9.c
+++ activity9.c
@@ -5,8 +5,14 @@ int main()
 int n,sum=0;
 printf(“Enter the total number of numbers\n”);
 scanf(“%d”,&n);
+int a[n];
 for(int i=0;i<n;i++)
-sum=sum+i;
+{
+printf(“Enter %d number”,(i+1));
+scanf(“%d”,&a[i]);
+sum=sum+a[i];
+}
 printf(“The final sum is %d\n”,sum);
 return 0;
 }
+
