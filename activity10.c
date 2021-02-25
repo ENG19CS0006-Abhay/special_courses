b1a30dacaea09c46c7708ec663c20e5ce36f947f
ENG19CS0006-Abhay <76053889+ENG19CS0006-Abhay@users.noreply.github.com>
2021-02-25 12:34:09 +0530

    Update activity10.c

--- activity10.c
+++ activity10.c
@@ -1,33 +1,32 @@
-//WAP to find the sum of n fractions.
 #include<stdio.h>
 struct fract
 {
-int  num;
-int  deno;
+   int  num;
+    int  deno;
 };
-typedef struct fract fraction;
+    typedef struct fract fraction;
 fraction input()
 {
-fraction a;
-printf("Enter the numerator\n");
-scanf("%d",&a.num);
-printf("Enter the denominator\n");
-scanf("%d",&a.deno);
-return a;
+   fraction a;
+   printf("Enter the numerator\n");
+   scanf("%d",&a.num);
+   printf("Enter the denominator\n");
+   scanf("%d",&a.deno);
+   return a;
 }
 fraction sum(fraction a,fraction b)
 {
-fraction res;
-if(a.deno==b.deno)
-{
-res.deno=a.deno;
-res.num=a.num+b.num;
-}
-else 
-{
-res.deno=a.deno*b.deno;
+  fraction res;
+  int gcd=1;
+ res.deno=a.deno*b.deno;
 res.num=(a.num*b.deno)+(b.num*a.deno);
+for(int i=1;i<=(res.num)&&i<=(res.deno);i++)
+{
+if((res.num)%i==0&&(res.deno)%i==0)
+gcd=i;
 }
+res.deno=(res.deno)/gcd;
+res.num=(res.num)/gcd;
 return res;
 }
 int main()
@@ -47,7 +46,6 @@ for(int i=0;i<n;i++)
 {
 c=sum(c,a[i]);
 }
-printf("The final sum is %d/%d which is %.2f %",c.num,c.deno,(c.num/(1.0*c.deno))*100);
+printf("The final sum is %d/%d ",c.num,c.deno);
 return 0;
 }
-//No errors
